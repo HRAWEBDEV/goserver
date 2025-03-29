db8d65b83f52168fa7c989d3c582d5c3cca9a124
hamidwise1 <hrawebdev@gmail.com>
2025-03-29 11:22:37 +0330

    add router and cors

--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"*"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+	log.Printf("Server started on port %s", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
